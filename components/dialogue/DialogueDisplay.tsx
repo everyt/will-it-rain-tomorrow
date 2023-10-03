e06842f723265d524117eb434ba21a70866f27e5
Everyhongcha <80094147+everyt@users.noreply.github.com>
2023-10-04 07:00:41 +0900

    make effect layer

--- components/dialogue/DialogueDisplay.tsx
+++ components/dialogue/DialogueDisplay.tsx
@@ -2,14 +2,16 @@
 
 import { useEffect, useRef, useState } from 'react';
 import useInterval from 'wirt@/hooks/useInterval';
+import { motion } from 'framer-motion';
 
 type DialogueDisplayProps = {
   flow: string;
   setFlow: Function;
   children: string;
   name?: string;
-  background?: string;
   title?: string;
+  effect?: string;
+  background?: string;
 };
 type letters = {
   letter: string;
@@ -21,86 +23,94 @@ type lettersMap = {
   letter: number;
 }
 
-export default function DialogueDisplay({ flow, setFlow, children, name, background, title }: DialogueDisplayProps) {
+export default function DialogueDisplay({ flow, setFlow, children, name, title, effect, background }: DialogueDisplayProps) {
   const [delay, setDelay] = useState(100);
 
-// 대화 분해용 React 변수 ㅡㅡ────────────────────────────────────────────────────┐┌────┐
-  const letters = useRef<letters[]>([]);                        //EveryHongCha ││ ┌──┘
-  const lettersMap = useRef<lettersMap[]>([]);                               //││ └──┐
+// 대화 분해용 React 변수 ㅡㅡ────────────────────────────────────────────────────┐┌─┐
+  const letters = useRef<letters[]>([]);                        //EveryHongCha │└─┘
+  const lettersMap = useRef<lettersMap[]>([]);                               //│┌────┐
   const [lettersMapState, setLettersMapState] = useState<lettersMap[]>([]);  //││ ┌──┘
   const [typingCount, setTypingCount] = useState<number>(0);                 //││ └──┐
-// Flow 관리자 ─────────────────────────────────────────────────────────────────┤└────┘
-  useEffect(() => {                                             //EveryHongCha │┌─┐┌─┐
-    if ( flow === 'next' ) {                                                 //││ ││ │
-      setFlow('start');                                                      //││ ││ │
+// Flow 관리자 ─────────────────────────────────────────────────────────────────┤│ ┌──┘
+  useEffect(() => {                                             //EveryHongCha ││ └──┐
+    if ( flow === 'next' ) {                                                 //│└────┘
+      setFlow('start');                                                      //│┌─┐┌─┐
     }                                                                        //││ ││ │
-    if (flow === 'end') {                                                    //││ └┘ │
-      setFlow('next');                                                       //│└────┘
-      setTypingCount(0);                                                     //│┌────┐
-      letters.current = [];                                                  //││ ┌──┘
-      lettersMap.current = [];                                               //││ └──┐
+    if (flow === 'end') {                                                    //││ ││ │
+      setFlow('next');                                                       //││ ││ │
+      setTypingCount(0);                                                     //││ └┘ │
+      letters.current = [];                                                  //│└────┘
+      lettersMap.current = [];                                               //│┌────┐
       setLettersMapState([]);                                                //││ ┌──┘
     }                                                                        //││ └──┐
-  }, [flow]);                                                                //│└────┘
-  useInterval(                                                               //│┌────┐
-    () => {                                                                  //││ ┌┐ │
-      console.log(flow);                                                     //││ └┘┌┘
-      setDelay(flow === 'skip' ? 0 : 100);                                   //││ ┌┐└┐
-      typing();                                                 //EveryHongCha ││ ││ │
-    }, // ─────────────────────────────────────────────────────────────────────┤└─┘└─┘
-    flow === 'start'    ? 0                           // start: 대화가 시작함    │┌─┐┌─┐
-  : flow === 'typing'   ? delay                       // typing: 타이핑         ││ ││ │
-  : flow === 'skip'     ? 0                           // skip: 스킵함           ││ └┘ │
-  : flow === 'delayEnd' ? 2000                        // delayEnd: 종료를 딜레이 │└──┐ │
-  : null,                                             // end: 종료함            │┌──┘ │
-  ); //                                                                        │└────┘
-// 타이핑 시스템 ────────────────────────────────────────────────────────────────┤┌─┐┌─┐
-  const typing = () => {                                        //EveryHongCha ││ ││ │
-    const initializeDialog = () => {                                         //││ └┘ │
-      if (children) {                                                        //││ ┌┐ │
+  }, [flow]);                                                                //││ ┌──┘
+  useInterval(                                                               //││ └──┐
+    () => {                                                                  //│└────┘
+      console.log(flow);                                                     //│┌────┐
+      setDelay(flow === 'skip' ? 0 : 100);                                   //││ ┌┐ │
+      typing();                                                 //EveryHongCha ││ └┘┌┘
+    }, // ─────────────────────────────────────────────────────────────────────┤│ ┌┐└┐
+    flow === 'start'    ? 0                           // EveryHongCha          ││ ││ │
+  : flow === 'effect'   ? 2000                        // start: 대화가 시작함    │└─┘└─┘
+  : flow === 'typing'   ? delay                       // effect: 이펙트         │┌─┐┌─┐
+  : flow === 'skip'     ? 0                           // typing: 타이핑         ││ ││ │
+  : flow === 'delayEnd' ? 2000                        // skip: 스킵함           ││ └┘ │
+  : null,                                             // delayEnd: 종료를 딜레이 │└──┐ │
+  );                                                  // end: 종료함            │┌──┘ │
+// 타이핑 시스템 ────────────────────────────────────────────────────────────────┤└────┘
+  const typing = () => {                                        //EveryHongCha │┌─┐
+    const initializeDialog = () => {                                         //│└─┘
+      if (children) {                                                        //│┌─┐┌─┐
         let tempLetters = children.split('');                                //││ ││ │
-        let letter = '';                                                     //│└─┘└─┘
-        for (let i = 0; i < tempLetters.length; i++) {                       //│┌────┐
-          letter = tempLetters[i];                                           //││ ┌┐ │
-          let type = 'text';                                                 //││ ││ │
-          if (tempLetters[i] === '<') {                                      //││ ││ │
-            i++;                                                             //││ └┘ │
-            type = tempLetters[i] === '/' ? 'closingTag' : 'startingTag';    //│└────┘
-            while (tempLetters[i] !== '>') {                                 //│┌────┐
-              letter += tempLetters[i];                                      //││ ┌┐ │
-              i++;                                                           //││ ││ │
-            }                                                                //││ ││ │
-            letter += tempLetters[i];                                        //││ ││ │
-          }                                                                  //│└─┘└─┘
-          if (letter === ' ') letter = '&nbsp;';                             //│┌────┐
-          letters.current.push({letter, type});                              //││ ┌──┘
-        }                                                                    //││ │┌─┐
-        for (let i= 0; i < letters.current.length; i++) {                    //││ ││ │
-          if (letters.current[i].type === 'text') {                          //││ └┘ │
-            lettersMap.current.push({prefix: null, suffix: null, letter: i}) //│└────┘
-          } else if (letters.current[i].type === 'startingTag') {            //│┌────┐
-            let j = 0;                                                       //││ ┌──┘
-            let k = i;                                                       //││ │
-            while (letters.current[i].type !== 'closingTag') {               //││ │
-              i++;                                                           //││ └──┐
-              j++;                                                           //│└────┘
-            }                                                                //│┌─┐┌─┐
-            const tempLettersMap: lettersMap[] = [];                         //││ ││ │
-            for (j; j > 1; j--) {                                            //││ └┘ │
-              tempLettersMap.push({prefix: k, suffix: i, letter: j + k - 1}) //││ ┌┐ │
-            }                                                                //││ ││ │
-            lettersMap.current.push(...tempLettersMap.reverse());            //│└─┘└─┘
-          }                                                                  //│┌────┐
-        }                                                                    //││ ┌┐ │
-        setLettersMapState(lettersMap.current);                              //││ └┘ │
-      } else {                                                               //││ ┌┐ │
-        console.log('※ DialogBox: children is null. ※');     　　            //││ ││ │
-      }                                                                      //│└─┘└─┘
-    }                                                                        //│┌─┐
-    if (flow === 'start') {                                                  //││ │
-      initializeDialog();                                                    //││ │
-      setFlow('typing');                                                     //││ │
-    } else if (flow === 'typing' || flow === 'skip') {                       //││ │
+        let letter = '';                                                     //││ └┘ │
+        for (let i = 0; i < tempLetters.length; i++) {                       //││ ┌┐ │
+          letter = tempLetters[i];                                           //││ ││ │
+          let type = 'text';                                                 //│└─┘└─┘
+          if (tempLetters[i] === '<') {                                      //│┌────┐
+            i++;                                                             //││ ┌┐ │
+            type = tempLetters[i] === '/' ? 'closingTag' : 'startingTag';    //││ ││ │
+            while (tempLetters[i] !== '>') {                                 //││ ││ │
+              letter += tempLetters[i];                                      //││ └┘ │
+              i++;                                                           //│└────┘
+            }                                                                //│┌────┐
+            letter += tempLetters[i];                                        //││ ┌┐ │
+          }                                                                  //││ ││ │
+          if (letter === ' ') letter = '&nbsp;';                             //││ ││ │
+          letters.current.push({letter, type});                              //││ ││ │
+        }                                                                    //│└─┘└─┘
+        for (let i= 0; i < letters.current.length; i++) {                    //│┌────┐
+          if (letters.current[i].type === 'text') {                          //││ ┌──┘
+            lettersMap.current.push({prefix: null, suffix: null, letter: i}) //││ │┌─┐
+          } else if (letters.current[i].type === 'startingTag') {            //││ ││ │
+            let j = 0;                                                       //││ └┘ │
+            let k = i;                                                       //│└────┘
+            while (letters.current[i].type !== 'closingTag') {               //│┌─┐
+              i++;                                                           //│└─┘
+              j++;                                                           //│┌────┐
+            }                                                                //││ ┌──┘
+            const tempLettersMap: lettersMap[] = [];                         //││ │
+            for (j; j > 1; j--) {                                            //││ │
+              tempLettersMap.push({prefix: k, suffix: i, letter: j + k - 1}) //││ └──┐
+            }                                                                //│└────┘
+            lettersMap.current.push(...tempLettersMap.reverse());            //│┌─┐┌─┐
+          }                                                                  //││ ││ │
+        }                                                                    //││ └┘ │
+        setLettersMapState(lettersMap.current);                              //││ ┌┐ │
+      } else {                                                               //││ ││ │
+        console.log('※ DialogBox: children is null. ※');     　　            //│└─┘└─┘
+      }                                                                      //│┌────┐
+    }                                                                        //││ ┌┐ │
+    const initailizeEffect = () => {
+      if (effect !== 'undefined') {                                          //││ └┘ │
+        
+      }
+    }
+    if (flow === 'start') {                                                  //│
+      initializeDialog();                                                    //││ ┌┐ │
+      setFlow(effect === 'undefined' ? 'typing' : 'effect');                 //││ ││ │
+    } else if (flow === 'effect') {                                          //││ ││ │
+      setFlow('typing');                                                     //│└─┘└─┘
+    } else if (flow === 'typing' || flow === 'skip') {                       //│┌─┐
       if (typingCount <= lettersMapState.length) {                           //││ │
         setTypingCount(prev => prev + 1);                                    //││ │
       } else {                                                               //││ │
@@ -128,12 +138,16 @@ export default function DialogueDisplay({ flow, setFlow, children, name, backgro
 
   return (
     <>
-      {background !== 'null' &&
+      {effect !== 'undefined' && (
+        <motion.div>
+        </motion.div>
+      )}
+      {background !== 'null' && (
         <img src={background}
           className='object-cover
           absolute left-0 top-0 z-10'
         />
-      }
+      )}
       <div
         className='h-1/6 w-full
         flex flex-col justify-end
--- components/dialogue/DialogueInput.tsx
+++ components/dialogue/DialogueInput.tsx
@@ -5,13 +5,16 @@ import { useRouter } from 'next/navigation';
 
 import DialogueDisplay from 'wirt@/components/dialogue/DialogueDisplay';
 
+type stringornull = string | null;
+
 export default function DialogueInput({ json }: { json: Object }) {
   const router = useRouter();
   const extractNameAndDialogue = (json: string) => {
-    const names: string[] = [];
-    const titles: string[] = [];
-    const dialogues: string[] = [];
-    const backgrounds: string[] = [];
+    const names: stringornull[] = [];
+    const titles: stringornull[] = [];
+    const effects: stringornull[] = [];
+    const dialogues: stringornull[] = [];
+    const backgrounds: stringornull[] = [];
 
     const data = JSON.parse(json);
 
@@ -26,6 +29,11 @@ export default function DialogueInput({ json }: { json: Object }) {
       } else {
         titles.push('undefined');
       }
+      if (item.effect) {
+        effects.push(item.effect);
+      } else {
+        effects.push('undefined');
+      }
       if (item.dialogue) {
         dialogues.push(item.dialogue);
       } else {
@@ -38,28 +46,29 @@ export default function DialogueInput({ json }: { json: Object }) {
       }
     });
 
-    return [titles, backgrounds, names, dialogues];
+    return [names, titles, effects, dialogues, backgrounds];
   };
-  const [isMounted, setIsMounted] = useState(false);
-  const [titles, backgrounds, names, dialogues] = extractNameAndDialogue(JSON.stringify(json));
+  const [names, titles, effects, dialogues, backgrounds] = extractNameAndDialogue(JSON.stringify(json));
   const [flow, setFlow] = useState('next');
   const [index, setIndex] = useState(0);
   const name = useRef<string>('');
   const title = useRef<string>('');
+  const effect = useRef<string>('');
   const dialogue = useRef<string>('');
   const background = useRef<string>('');
 
   useEffect(() => {
     const isNext = () => {
       if (flow === 'next') {
+        if (    effects[index] !== 'undefined') effect.current = effects[index]!;
         if (  dialogues[index] ===  undefined  && index !== 0) router.push('/');
-        if (backgrounds[index] !== 'undefined') background.current = backgrounds[index]
-        if (     titles[index] !== 'undefined') title.current = titles[index]
-        if (      names[index] !== 'undefined') name.current = names[index];
+        if (backgrounds[index] !== 'undefined') background.current = backgrounds[index]!;
+        if (     titles[index] !== 'undefined') title.current = titles[index]!;
+        if (      names[index] !== 'undefined') name.current = names[index]!;
         if        (dialogues[index] === 'undefined' && index === 0) {
-          dialogue.current = dialogues[index+1]
+          dialogue.current = dialogues[index+1]!;
         } else if (dialogues[index] !== 'undefined' && index !== 0) {
-          dialogue.current = dialogues[index]
+          dialogue.current = dialogues[index]!;
         }
         setIndex((prev) => prev + 1);
       }
@@ -73,6 +82,7 @@ export default function DialogueInput({ json }: { json: Object }) {
      setFlow={setFlow}
      name={name.current}
      title={title.current}
+     effect={effect.current}
      background={background.current}
     >
       {dialogue.current}
