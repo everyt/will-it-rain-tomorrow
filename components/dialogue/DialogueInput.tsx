963089124340073b3baac1bb64ca13704fc456ec
everyt <everytea.time@gmail.com>
2023-10-04 14:56:30 +0900

    clean code

--- components/dialogue/DialogueInput.tsx
+++ components/dialogue/DialogueInput.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { motion } from 'framer-motion';
 
 import DialogueDisplay from 'wirt@/components/dialogue/DialogueDisplay';
 
@@ -12,7 +13,6 @@ export default function DialogueInput({ json }: { json: Object }) {
   const extractNameAndDialogue = (json: string) => {
     const names: stringornull[] = [];
     const titles: stringornull[] = [];
-    const effects: stringornull[] = [];
     const dialogues: stringornull[] = [];
     const backgrounds: stringornull[] = [];
 
@@ -22,53 +22,46 @@ export default function DialogueInput({ json }: { json: Object }) {
       if (item.name) {
         names.push(item.name);
       } else {
-        names.push('undefined');
+        names.push(null);
       }
       if (item.title) {
         titles.push(item.title);
       } else {
-        titles.push('undefined');
-      }
-      if (item.effect) {
-        effects.push(item.effect);
-      } else {
-        effects.push('undefined');
+        titles.push(null);
       }
       if (item.dialogue) {
         dialogues.push(item.dialogue);
       } else {
-        dialogues.push('undefined');
+        dialogues.push(null);
       }
       if (item.background) {
         backgrounds.push(item.background);
       } else {
-        backgrounds.push('undefined');
+        backgrounds.push(null);
       }
     });
 
-    return [names, titles, effects, dialogues, backgrounds];
+    return [names, titles, dialogues, backgrounds];
   };
-  const [names, titles, effects, dialogues, backgrounds] = extractNameAndDialogue(JSON.stringify(json));
+  const [names, titles, dialogues, backgrounds] = extractNameAndDialogue(JSON.stringify(json));
   const [flow, setFlow] = useState('next');
   const [index, setIndex] = useState(0);
   const name = useRef<string>('');
   const title = useRef<string>('');
-  const effect = useRef<string>('');
   const dialogue = useRef<string>('');
   const background = useRef<string>('');
 
   useEffect(() => {
     const isNext = () => {
       if (flow === 'next') {
-        if (    effects[index] !== 'undefined') effect.current = effects[index]!;
-        if (  dialogues[index] ===  undefined  && index !== 0) router.push('/');
-        if (backgrounds[index] !== 'undefined') background.current = backgrounds[index]!;
-        if (     titles[index] !== 'undefined') title.current = titles[index]!;
-        if (      names[index] !== 'undefined') name.current = names[index]!;
-        if        (dialogues[index] === 'undefined' && index === 0) {
-          dialogue.current = dialogues[index+1]!;
-        } else if (dialogues[index] !== 'undefined' && index !== 0) {
-          dialogue.current = dialogues[index]!;
+        if (  dialogues[index] === undefined && index !== 0) router.push('/');
+        if (backgrounds[index] !== null) background.current = backgrounds[index]!;
+        if (     titles[index] !== null)      title.current = titles[index]!;
+        if (      names[index] !== null)       name.current = names[index]!;
+        if        (dialogues[index] === null && index === 0) {
+                                           dialogue.current = dialogues[index+1]!;
+        } else if (dialogues[index] !== null && index !== 0) {
+                                           dialogue.current = dialogues[index]!;
         }
         setIndex((prev) => prev + 1);
       }
@@ -77,15 +70,22 @@ export default function DialogueInput({ json }: { json: Object }) {
   }, [flow]);
 
   return (
+    <>
+    <motion.div
+      className='absolute left-0 top-0 z-30 h-screen w-screen bg-black'
+      initial={{ opacity: 1 }}
+      animate={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    />
     <DialogueDisplay
      flow={flow}
      setFlow={setFlow}
      name={name.current}
      title={title.current}
-     effect={effect.current}
      background={background.current}
     >
       {dialogue.current}
     </DialogueDisplay>
+    </>
   );
 }
